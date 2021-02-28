4f30131966dd7c43c90a48f8ca9baeddd96a3423
NOSH2000 <nowshinsayara2000@gmail.com>
2021-02-28 16:46:49 -0500

    Add character count and limit

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -5,10 +5,13 @@ import okhttp3.Headers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -20,10 +23,11 @@ import org.parceler.Parcels;
 public class ComposeActivity extends AppCompatActivity {
 
     public static final String TAG = "ComposeActivity";
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 280;
 
     EditText etCompose;
     Button btnTweet;
+    TextView tvCount;
 
     TwitterClient client;
 
@@ -36,6 +40,28 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCount = findViewById(R.id.tvCount);
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+                int num = etCompose.getText().toString().length();
+                Log.i(TAG, "onTextChanged: " + num);
+                tvCount.setText("" + (int) num);
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+            }
+        });
 
         //Set click listener on button
         btnTweet.setOnClickListener(new View.OnClickListener() {
@@ -43,11 +69,11 @@ public class ComposeActivity extends AppCompatActivity {
             public void onClick(View v) {
                 // If the tweet is empty or more than the character limit
                 final String tweetContent = etCompose.getText().toString();
-                if(tweetContent.isEmpty()) {
+                if (tweetContent.isEmpty()) {
                     Toast.makeText(ComposeActivity.this, "Sorry, your tweet cannot be empty", Toast.LENGTH_LONG).show();
                     return;
                 }
-                if(tweetContent.length() > MAX_TWEET_LENGTH){
+                if (tweetContent.length() > MAX_TWEET_LENGTH) {
                     Toast.makeText(ComposeActivity.this, "Sorry, your tweet is too long", Toast.LENGTH_LONG).show();
                     return;
                 }
